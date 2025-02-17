6fc39fe8a0b486bd81a35a2bd357f856c3260cd5
Philippe-André De Carvalho <philippe.andre.decarvalho@gmail.com>
2025-02-17 20:25:52 +0100

    feat: add copier answer file and init package file

--- tests/test_template.py
+++ tests/test_template.py
@@ -2,10 +2,12 @@
 
 from copier import run_copy
 from pathlib import Path
+import subprocess
 
 
 DATA_SAMPLE = {
     "project_name": "test-project",
+    "package_name": "test_project",
 }
 
 
@@ -14,18 +16,42 @@ def test_template_generation() -> None:
     output_dir = Path("./output")
 
     run_copy(
-        src_path="template",
+        src_path=".",
         dst_path=output_dir,
         data=DATA_SAMPLE,
         vcs_ref="HEAD",
         unsafe=True,
     )
 
+    # Expected files after first generation
     expected_files = [
-        "README.md",
+        f"src/{DATA_SAMPLE['project_name'].replace('-', '_')}/__init__.py",
+        ".copier-answers.yml",
         "pyproject.toml",
+        "README.md",
     ]
 
     for file_path in expected_files:
+        full_path = output_dir / file_path
+        assert full_path.exists()
+
+    # Run poetry install
+    result = subprocess.run(
+        ["poetry", "install"],
+        cwd=output_dir,
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+
+    # Check if command was successful
+    assert result.returncode == 0, f"Poetry install failed with error: {result.stderr}"
+
+    # Expected files after poetry install
+    expected_files_after_install = [
+        "poetry.lock",
+    ]
+
+    for file_path in expected_files_after_install:
         full_path = output_dir / file_path
         assert full_path.exists()
